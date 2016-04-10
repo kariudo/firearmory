2bc661bee73f8c7a704865c75fe0ccea65d9d836
Hunter Horsman <kariudo@gmail.com>
2016-04-10 17:16:34 -0400

    Add definition for edit button icon

--- public/js/app.js
+++ public/js/app.js
@@ -11,10 +11,13 @@ config(function($routeProvider, $locationProvider, $resourceProvider, $mdIconPro
         });
     $locationProvider.html5Mode(true);
     $resourceProvider.defaults.stripTrailingSlashes = false;
+
     $mdIconProvider.icon('menu', '/img/ic_menu_24px.svg', 24);
     $mdIconProvider.icon('collection', '/img/ic_book_open_24px.svg', 24);
     $mdIconProvider.icon('range', '/img/ic_target_24px.svg', 24);
     $mdIconProvider.icon('more', '/img/ic_more_vert_24px.svg', 24);
+    $mdIconProvider.icon('edit', '/img/ic_mode_edit_24px.svg', 24);
+    
     $mdThemingProvider.theme('default')
         .primaryPalette('deep-purple')
         .accentPalette('lime');
